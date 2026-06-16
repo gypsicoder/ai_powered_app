c7defb31cc99e67b493af61a0e3b218e6751d275
Bishnu Pada Chanda <gypsi.coder@gmail.com>
2026-06-17 01:58:17 +0200

    Setup prettier for code formatting

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,9 +11,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
