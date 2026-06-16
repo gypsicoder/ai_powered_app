35c4943d825da0bef248c7d9fab4155c674ddbca
Bishnu Pada Chanda <gypsi.coder@gmail.com>
2026-06-17 01:23:00 +0200

    Added script to run both apps together from root project

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
